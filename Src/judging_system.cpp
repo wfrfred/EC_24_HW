5d5d7d3736d741c80c637cf178803c455c93f347
wfrfred <wfrfred@gmail.com>
2023-10-22 19:41:55 +0800

    feat: 写了一半的命中检测

--- Src/judging_system.cpp
+++ Src/judging_system.cpp
@@ -2,15 +2,13 @@
 // Created by wfrfred on Oct.22 2023
 //
 
-#ifndef JUDGING_SYSTEM_H
-#define JUDGING_SYSTEM_H
-
 #include "../Inc/judging_system.h"
 
 #include <iostream>
 
 int loop(JudgingSystem* judgingSystem) {
     judgingSystem->time++;
+    hitDetect(judgingSystem);
     if (judgingSystem->base_b->hp == 0) {
         std::cout << "Red group wins!" << std::endl;
         return 0;
@@ -22,4 +20,23 @@ int loop(JudgingSystem* judgingSystem) {
     };
 }
 
-#endif
+inline void hitDetect(JudgingSystem* judgingSystem) {
+    if (judgingSystem->table->element == NULL) {
+        return;
+    }
+    while (judgingSystem->table->element->time == judgingSystem->time) {
+        for (;;) {
+            //TODO
+        }
+        judgingSystem->table = (judgingSystem->table);
+    }
+}
+
+inline float getDistance(TargetPoint* targetPoint, Robot* robot) {
+    float x1 = targetPoint->target_x;
+    float x2 = robot->pos->x;
+    float y1 = targetPoint->target_y;
+    float y2 = robot->pos->y;
+
+    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+}
